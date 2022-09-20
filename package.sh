862a8a69f5aac9318c326301b3a292afc42fe4db
Stewart Bennell <stewart@bennell.me>
2022-09-20 12:09:02 +1000

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,16 +3,16 @@ set -Eeuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1
 
 fpm -s dir -t deb --force \
-	-n pve-fake-subscription \
+	-n pve-bennellit-subscription \
 	--description "Pollute the subscription cache of Proxmox VE (>=5.0), Proxmox Mail Gateway (>=5.0) & Proxmox Backup Server (>=1.0) so it won't alert you on dashboard login" \
-	--url "https://github.com/sbennell/pve-fake-subscription" \
+	--url "https://www.bennellit.com.au" \
 	-v 0.0.8 \
 	--license "GLWTS(Good Luck With That Shit) Public License" \
 	--depends "python3" \
 	--architecture all \
 	--deb-dist "unstable" \
 	--deb-priority "optional" \
-	--deb-systemd "usr/lib/systemd/system/pve-fake-subscription.timer" \
+	--deb-systemd "usr/lib/systemd/system/pve-bit-subscription.timer" \
 	--deb-systemd-enable --deb-systemd-auto-start --deb-systemd-restart-after-upgrade \
 	--after-remove "scripts/purge" \
 	./usr
